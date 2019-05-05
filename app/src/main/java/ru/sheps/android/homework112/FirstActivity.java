6a1533756f3fc9723342fb801cb20df868405f67
Katya Sheps <katya@sheps.ru>
2019-05-05 19:42:27 +0300

    Create SplashScreenActivity

--- app/src/main/java/ru/sheps/android/homework112/FirstActivity.java
+++ app/src/main/java/ru/sheps/android/homework112/FirstActivity.java
@@ -3,11 +3,11 @@ package ru.sheps.android.homework112;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 
-public class MainActivity extends AppCompatActivity {
+public class FirstActivity extends AppCompatActivity {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_main);
+        setContentView(R.layout.activity_first);
     }
 }
